3fa652c43c4d5ad88bb9a4ed281dee1d66e84310
Hosung Kim <hk63@rice.edu>
2024-11-24 16:33:12 -0600

    swift initial

---
+++ PilloraClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PilloraClient
+//
+//  Created by Hosung Kim on 11/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PilloraClient/PilloraClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PilloraClientApp.swift
+//  PilloraClient
+//
+//  Created by Hosung Kim on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PilloraClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
